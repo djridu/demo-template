53914f5819324d34c3086b8c1262c17272598c48
Maksim Zima <maksimz@wizardsdev.com>
2019-02-18 11:27:24 +0200

    add demo storybook

---
+++ src/Components/Button/Button.stories.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Button from './Button';
+
+storiesOf('Button with image', module).add('with text', () => <Button>Hello Button</Button>);
